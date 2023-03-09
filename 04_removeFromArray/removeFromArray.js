cda2c9d302d236b402b09c1af7de84f136a2837d
Jeremiah <jeremiahjacobson10@gmail.com>
2023-03-09 18:28:21 -0500

    finish javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array1, ...targets) {
+  for (let i = 0; i < targets.length; ) {
+    const index = array1.indexOf(targets[i]);
+    if (index != -1) {|
+      array1.splice(index, 1);
+    }
+  }
+  return array1;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+// Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
 
+// ```javascript
+// sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+// ```
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
+  let larger;
+  let smaller;
+
+  if (num1 > num2) {
+    larger = num1;
+    smaller = num2;
+  } else {
+    larger = num2;
+    smaller = num1;
+  }
+  for (let i = smaller; i <= larger; i++) {
+    sum += i;
+  }
+  return sum;
 };
-
+// console.log(sumAll(123, 1));
 // Do not edit below this line
 module.exports = sumAll;
