31f0501e2fb91d1a55595c0169dd20cfd5fce248
Jeremiah <jeremiahjacobson10@gmail.com>
2023-03-08 21:36:27 -0500

    complete javascript problem solving exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let outputString = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      outputString += string;
+    }
+    return outputString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
